8d802a4254fc854804bbaaa44ea9eb97e2cbec50
rettynova <silvertag92@gmail.com>
2024-11-14 19:16:14 +0900

    add "all files"

---
+++ dist/protobuf/packetNames.js
@@ -0,0 +1,11 @@
+export const packetNames = {
+    packet: {
+        GamePacket: 'packet.GamePacket',
+    },
+    request: {
+        C2SRegisterRequest: 'request.C2SRegisterRequest',
+    },
+    response: {
+        S2CRegisterResponse: 'response.S2CRegisterResponse',
+    },
+};
