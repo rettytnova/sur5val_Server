74b45bc94e2e51d69b9aae2e2c43788839a71f8a
성원 정 <tyhg56@naver.com>
2024-11-15 00:06:32 +0900

    fix : " config.ts 수정 "
    config
     - 상수값 추가

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,10 @@
 const PORT : number = 5555;
 const HOST : string = '127.0.0.1';
 
+export const TOTAL_LENGTH = 11;
+export const VERSION_START = 3;
+export const SEQUENCE_SIZE = 4;
+
 export const config={
     server:{
         port:PORT,
