a38053ac252ba3d2f7761d65de31c84d8c33e0ff
성원 정 <tyhg56@naver.com>
2024-11-14 22:03:36 +0900

    fix : " server.ts 수정 "
    server.ts
     - clientConnection() 추가
      - 클라 연결 후 이벤트 할당
       - data, end, error 이벤트
    
     - listen() 추가
      - 서버 리슨
    
     - start() 추가
      - 서버 시작

--- src/class/server.ts
+++ src/class/server.ts
@@ -1,12 +1,23 @@
 import fs from 'fs';
 import path from 'path';
+import net from "net";
 import { fileURLToPath } from 'url';
 import { packetNames } from '../protobuf/packetNames.js';
 import protobuf from 'protobufjs';
+import { config } from '../config/config.js';
+import { onData } from '../events/onData.js';
+import { onEnd } from '../events/onEnd.js';
+import { onError } from '../events/onError.js';
+
+interface SocketWithBuffer extends net.Socket { buffer: Buffer }
 
 class Server {
-  private protoMessages: { [key: string]: any } = {};
-  constructor() {}
+  private protoMessages: { [key: string]: any } = {};  
+  private server: net.Server;
+
+  constructor() {
+    this.server = net.createServer();
+  }
 
   getDir(): string {
     const __filename: string = fileURLToPath(import.meta.url);
@@ -55,6 +66,32 @@ class Server {
     } catch (err) {
       console.error('Protobuf 파일 로드 중 오류가 발생했습니다.', err);
     }
+  }
+  
+  clientConnection(socket : net.Socket)
+  {
+    console.log(`Client connected from: ${socket.remoteAddress}:${socket.remotePort}`);
+    
+    const customSocket = socket as SocketWithBuffer;
+    customSocket.buffer = Buffer.alloc(0);
+
+    socket.on('data', onData(customSocket));
+    socket.on('end', onEnd(customSocket));
+    socket.on('error', onError(customSocket));
+  }
+
+  listen () {
+    this.server.listen(config.server.port, config.server.host, () => {
+      console.log(`서버가 ${config.server.host}:${config.server.port}에서 실행 중입니다.`);
+      console.log(this.server.address());
+    });
+  }
+
+  start()
+  {
+    this.initializeProto();
+    
+    this.listen();
   }
 }
 
