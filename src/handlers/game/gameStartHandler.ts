a77068e6c7300b5bc1ce3b9c80c1b799ef4afd64
성원 정 <tyhg56@naver.com>
2024-11-15 17:22:00 +0900

    fix : " gameStartHandler 수정 "
    gameStartHandler
     - payload 매개변수 자료형을 object | null 에서 object로 변경

--- src/handlers/game/gameStartHandler.ts
+++ src/handlers/game/gameStartHandler.ts
@@ -1,5 +1,5 @@
 import net from "net"
 
-export const gameStartHandler = (socket: net.Socket, payload: Object | null) => {
+export const gameStartHandler = (socket: net.Socket, payload: Object) => {
 
 }
