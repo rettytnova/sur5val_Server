085a6ef9c18e7450f2b123a21e157a12f6682c3e
성원 정 <tyhg56@naver.com>
2024-11-16 22:24:27 +0900

    fix : " interface 수정 "
    interface
     - Card, CharacterStateinfo, Character, Uset, Room, CreateRoomPayload  추가

--- src/interface/interface.ts
+++ src/interface/interface.ts
@@ -2,5 +2,50 @@ import net from 'net';
 
 export interface CustomSocket extends net.Socket {
     buffer: Buffer,
-    id? : string
+    id?: string
+}
+
+export interface CreateRoomPayload {
+    roomName: string,
+    maxUserNum: Number
+}
+
+export interface Card{
+    type:Number;
+    count:Number;
+}
+
+export interface CharacterStateInfo{
+    state : Number;
+    nextState : Number; 
+    nextStateAt : Number;
+    stateTargetUserId: Number;
+}
+
+export interface Character {
+    characterType: Number;
+    roleType: Number;
+    hp: Number;
+    weapon: Number;
+    stateInfo:CharacterStateInfo;
+    equips: Number[];
+    debuffs: Number[];
+    handCards: Card[];
+    bbangCount: Number;
+    handCardsCount: Number;
+}
+
+export interface User {
+    id: Number;
+    nickName: string;
+    character: Character;
+}
+
+export interface Room{
+    id:Number;
+    ownerId:Number;
+    name:string;
+    maxUserNum:Number;
+    state:Number;
+    users:User[];
 }
