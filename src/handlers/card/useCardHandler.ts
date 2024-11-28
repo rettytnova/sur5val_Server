f48380d7a2e92d4049f47b07f79eb35897497b35
applaud13 <applaud13@gmail.com>
2024-11-28 20:09:50 +0900

    add equip, weapon
    equip장비와 weapon장비 장착 로직 생성 및 기본 장비들 생성

--- src/handlers/card/useCardHandler.ts
+++ src/handlers/card/useCardHandler.ts
@@ -1,11 +1,30 @@
 import { config } from '../../config/config.js';
-import { CustomSocket, UseCardRequest, UseCardResponse, Room, User, Character } from '../../interface/interface.js';
+import { CustomSocket, UseCardRequest, UseCardResponse, Room, User } from '../../interface/interface.js';
 import { CardType, GlobalFailCode } from '../enumTyps.js';
 import { sendPacket } from '../../packet/createPacket.js';
-import { getRedisData, getUserBySocket, setRedisData } from '../../handlers/handlerMethod.js';
+import { getRedisData, getRoomByUserId, getUserBySocket, setRedisData } from '../../handlers/handlerMethod.js';
 import { userUpdateNotification } from '../notification/userUpdate.js';
 import { socketSessions } from '../../session/socketSession.js';
-import { animationDelay, monsterAiDatas } from '../coreMethod/monsterMove.js';
+import { monsterAiDatas } from '../coreMethod/monsterMove.js';
+
+// DB에 들어갈 내용
+const DBEquip: { [cardType: number]: { attack: number; armor: number; hp: number } } = {
+  301: { attack: 0, armor: 0, hp: 0 },
+  302: { attack: 0, armor: 0, hp: 0 },
+  303: { attack: 0, armor: 0, hp: 0 },
+  304: { attack: 0, armor: 0, hp: 0 },
+  305: { attack: 0, armor: 0, hp: 0 },
+  306: { attack: 2, armor: 0, hp: 0 },
+  307: { attack: 0, armor: 0, hp: 8 },
+  308: { attack: 0, armor: 2, hp: 2 },
+  309: { attack: 0, armor: 1, hp: 5 },
+  310: { attack: 1, armor: 1, hp: 0 },
+  311: { attack: 4, armor: 0, hp: 0 },
+  312: { attack: 0, armor: 0, hp: 16 },
+  313: { attack: 0, armor: 4, hp: 4 },
+  314: { attack: 0, armor: 2, hp: 10 },
+  315: { attack: 2, armor: 2, hp: 0 }
+};
 
 const { packetType } = config;
 /***
@@ -68,15 +87,6 @@ export const useCardHandler = async (socket: CustomSocket, payload: Object): Pro
     }
     if (user === null) return;
 
-    // user의 캐릭터 스탯 정보 찾기
-    const characterStats: { [roomId: number]: { [userId: number]: Character } } | undefined =
-      await getRedisData('userStatusData');
-    if (characterStats === undefined) {
-      console.error('userStatusData 정보가 존재하지 않습니다.');
-      return;
-    }
-    const characterStat = characterStats[room.id][user.id];
-
     // target 정보 찾기
     let target: User | null = null;
     for (let i = 0; i < room.users.length; i++) {
@@ -96,41 +106,39 @@ export const useCardHandler = async (socket: CustomSocket, payload: Object): Pro
 
       // 이름: 쌍둥이 폭팔
       // 설명: 신비로운 마법의 에너지가 두 번 적을 빠르게 베어낸다. 두 번째 타격은 마법의 폭발로 적을 더 강하게 공격한다.
-      case CardType.MAGICIAN_BASIC_SKILL:
+      case CardType.MAGICIAN_BASIC_SKILL: {
         // 유효성 검사
         if (!target) return;
-        if (characterStat.mp < 2) {
+        if (user.character.mp < 2) {
           console.log('마나가 부족합니다.');
           return;
         }
 
         // 스킬 실행1
-        if (await attackTarget(user, rooms, room, 1, target, characterStats)) return;
+        if (await attackTarget(user, rooms, room, 1, target)) return;
         let index: number | null = null;
         for (let i = 0; i < monsterAiDatas[room.id].length; i++) {
           if (monsterAiDatas[room.id][i].id === target.id) {
-            monsterAiDatas[room.id][i].animationDelay = animationDelay;
+            monsterAiDatas[room.id][i].animationDelay = 5;
             index = i;
             break;
           }
         }
         if (index === null) return;
-        sendAnimation(user, user, 1);
-        sendAnimation(user, target, 2);
+        sendAnimation(user, target, 1);
 
         // 스킬 실행2
         setTimeout(async () => {
-          await attackTarget(user, rooms, room, 1.5, target, characterStats);
-          monsterAiDatas[room.id][index].animationDelay = animationDelay;
-          sendAnimation(user, user, 1);
-          sendAnimation(user, target, 2);
-        }, 600);
+          await attackTarget(user, rooms, room, 1.5, target);
+          monsterAiDatas[room.id][index].animationDelay = 5;
+          sendAnimation(user, target, 1);
+        }, 800);
 
         // 마나 소모
-        characterStat.mp -= 2;
-        await setRedisData('userStatusData', characterStats);
+        user.character.mp -= 2;
+        await setRedisData('roomData', rooms);
         break;
-
+      }
       // 이름: 전사 기본 스킬
       // 설명: 파티원의 방어력과 공격력을 올려주는 버프를 시전한다.
       case CardType.WARRIOR_BASIC_SKILL:
@@ -149,12 +157,48 @@ export const useCardHandler = async (socket: CustomSocket, payload: Object): Pro
         console.log('팔라딘 기본 스킬 사용!!!!');
         break;
 
-      // 이름: 마법사 강화 스킬
-      // 설명:
-      case CardType.MAGICIAN_EXTENDED_SKILL:
-        console.log('마법사 강화 스킬 사용!!!!');
-        break;
+      // 이름: 삼중 타격
+      // 설명: 푸른빛, 보랏빛, 붉은 폭발로 적을 강타한다. 타격마다 에너지가 증폭되어 최종 타격은 압도적인 파괴력을 발휘한다.
+      case CardType.MAGICIAN_EXTENDED_SKILL: {
+        // 유효성 검사
+        if (!target) return;
+        if (user.character.mp < 3) {
+          console.log('마나가 부족합니다.');
+          return;
+        }
 
+        // 스킬 실행1
+        if (await attackTarget(user, rooms, room, 1.2, target)) return;
+        let index: number | null = null;
+        for (let i = 0; i < monsterAiDatas[room.id].length; i++) {
+          if (monsterAiDatas[room.id][i].id === target.id) {
+            monsterAiDatas[room.id][i].animationDelay = 4;
+            index = i;
+            break;
+          }
+        }
+        if (index === null) return;
+        sendAnimation(user, target, 1);
+
+        // 스킬 실행2
+        setTimeout(async () => {
+          await attackTarget(user, rooms, room, 1.8, target);
+          monsterAiDatas[room.id][index].animationDelay = 4;
+          sendAnimation(user, target, 1);
+        }, 600);
+
+        // 스킬 실행3
+        setTimeout(async () => {
+          await attackTarget(user, rooms, room, 2.7, target);
+          monsterAiDatas[room.id][index].animationDelay = 4;
+          sendAnimation(user, target, 1);
+        }, 1200);
+
+        // 마나 소모
+        user.character.mp -= 3;
+        await setRedisData('roomData', rooms);
+        break;
+      }
       // 이름: 전사 강화 스킬
       // 설명: 일시적(10초간)으로 방어력이 대폭 상승한다.
       case CardType.WARRIOR_EXTENDED_SKILL:
@@ -183,10 +227,6 @@ export const useCardHandler = async (socket: CustomSocket, payload: Object): Pro
       // 설명:
       case CardType.BASIC_HP_POTION:
         usePotion(user, 1, 1, rooms, room, CardType.BASIC_HP_POTION);
-        // sendPacket(socket, config.packetType.USE_CARD_RESPONSE, {
-        //   success: true,
-        //   failCode: GlobalFailCode.NONE
-        // });
         break;
 
       // 이름:
@@ -252,27 +292,62 @@ export const useCardHandler = async (socket: CustomSocket, payload: Object): Pro
 
       // 이름:
       // 설명:
-      case CardType.NONE:
+      case CardType.BASIC_WEAPON:
+        equipWeapon(user, rooms, room, CardType.BASIC_WEAPON);
+        break;
+
+      // 이름: 초심자의 투구
+      // 설명:
+      case CardType.BASIC_HEAD:
+        equipItem(user, rooms, room, 0, CardType.BASIC_HEAD);
+        break;
+
+      // 이름: 초심자의 갑옷
+      // 설명:
+      case CardType.BASIC_ARMOR:
+        equipItem(user, rooms, room, 1, CardType.BASIC_ARMOR);
+        break;
+
+      // 이름: 초심자의 망토
+      // 설명:
+      case CardType.BASIC_CLOAK:
+        equipItem(user, rooms, room, 2, CardType.BASIC_CLOAK);
+        break;
+
+      // 이름: 초심자의 장갑
+      // 설명:
+      case CardType.BASIC_GLOVE:
+        equipItem(user, rooms, room, 3, CardType.BASIC_GLOVE);
         break;
 
       // 이름:
       // 설명:
-      case CardType.NONE:
+      case CardType.ADVANCED_WEAPON:
+        equipWeapon(user, rooms, room, CardType.ADVANCED_WEAPON);
         break;
 
       // 이름:
       // 설명:
-      case CardType.NONE:
+      case CardType.ADVANCED_HEAD:
+        equipItem(user, rooms, room, 0, CardType.ADVANCED_HEAD);
         break;
 
       // 이름:
       // 설명:
-      case CardType.NONE:
+      case CardType.ADVANCED_ARMOR:
+        equipItem(user, rooms, room, 1, CardType.ADVANCED_ARMOR);
         break;
 
       // 이름:
       // 설명:
-      case CardType.NONE:
+      case CardType.ADVANCED_CLOAK:
+        equipItem(user, rooms, room, 2, CardType.ADVANCED_CLOAK);
+        break;
+
+      // 이름:
+      // 설명:
+      case CardType.ADVANCED_GLOVE:
+        equipItem(user, rooms, room, 3, CardType.ADVANCED_GLOVE);
         break;
 
       // 이름:
@@ -296,7 +371,7 @@ export const useCardHandler = async (socket: CustomSocket, payload: Object): Pro
         break;
     }
 
-    console.log('남은 마나: ', characterStat.mp);
+    console.log('남은 마나: ', user.character.mp);
   } catch (error) {
     console.error(`useCardHandler ${error as Error}`);
   }
@@ -331,29 +406,21 @@ const changeStatus = async (
   }
 
   // 마나가 충분한지 검사
-  const characterStats: { [roomId: number]: { [userId: number]: Character } } | undefined =
-    await getRedisData('userStatusData');
-  if (characterStats === undefined) {
-    console.error('userStatusData 정보가 존재하지 않습니다.');
-    return;
-  }
-  const characterStat = characterStats[room.id][user.id];
-  if (characterStat.mp < manaCost) {
+  if (user.character.mp < manaCost) {
     console.log('마나가 부족합니다.');
     return;
   }
 
   // 버프 스킬 실행
-  characterStat.mp -= manaCost;
+  user.character.mp -= manaCost;
   if (user.character.hp + hp <= user.character.maxHp) {
     user.character.hp += hp;
   } else {
     user.character.hp = user.character.maxHp;
   }
-  characterStat.armor += armor;
-  characterStat.attack += attack;
+  user.character.mp += armor;
+  user.character.mp += attack;
 
-  await setRedisData('userStatusData', characterStats);
   await setRedisData('roomData', rooms);
   userUpdateNotification(room);
 };
@@ -370,22 +437,14 @@ const partyBuff = async (
   attack: number
 ) => {
   // 캐릭터 / 유저 정보 검사
-  const characterStats: { [roomId: number]: { [userId: number]: Character } } | undefined =
-    await getRedisData('userStatusData');
-  if (characterStats === undefined) {
-    console.error('userStatusData 정보가 존재하지 않습니다.');
-    return;
-  }
-
   // 마나가 충분한지 검사
-  const characterStat = characterStats[room.id][user.id];
-  if (characterStat.mp < manaCost) {
+  if (user.character.mp < manaCost) {
     console.log('마나가 부족합니다.');
     return;
   }
 
   // 버프 스킬 실행
-  characterStat.mp -= manaCost;
+  user.character.mp -= manaCost;
   for (let i = 0; i < room.users.length; i++) {
     if (room.users[i].character.roleType === 2) {
       if (user.character.hp + hp <= user.character.maxHp) {
@@ -393,25 +452,17 @@ const partyBuff = async (
       } else {
         user.character.hp = user.character.maxHp;
       }
-      characterStats[room.id][room.users[i].id].armor += armor;
-      characterStats[room.id][room.users[i].id].attack += attack;
+      user.character.armor += armor;
+      user.character.attack += attack;
     }
   }
-  await setRedisData('userStatusData', characterStats);
   await setRedisData('roomData', rooms);
   userUpdateNotification(room);
 };
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 // 타겟이 된 적 공격 (적군 체력 감소)
-const attackTarget = async (
-  attacker: User,
-  rooms: Room[],
-  room: Room,
-  skillCoeffcient: number,
-  target: User,
-  characterStats: { [roomId: number]: { [userId: number]: Character } }
-) => {
+const attackTarget = async (attacker: User, rooms: Room[], room: Room, skillCoeffcient: number, target: User) => {
   // 적군이 선택되었는지 검사
   if (target.character.roleType === 2) {
     console.error('적군에게만 사용할 수 있는 스킬입니다.');
@@ -423,12 +474,24 @@ const attackTarget = async (
     return true;
   }
 
+  // 공격 실행 중 라운드가 바뀌지 않았는지 검사
+  let isChanged: boolean = true;
+  const roomNow = await getRoomByUserId(attacker.id);
+  if (!roomNow) return;
+  for (let i = 0; i < roomNow.users.length; i++) {
+    if (roomNow.users[i].id === target.id) isChanged = false;
+  }
+  if (isChanged) {
+    console.log('공격 실행 중 라운드 바뀌어서 몬스터 사라짐');
+    return;
+  }
+
   // 공격 스킬 실행
-  const damage = Math.round(characterStats[room.id][attacker.id].attack * skillCoeffcient);
-  target.character.hp -= damage;
+  const damage = Math.round(attacker.character.attack * skillCoeffcient - target.character.armor);
+  target.character.hp -= Math.max(damage, 0);
   if (target.character.hp < 0) target.character.hp = 0;
-  await setRedisData('roomData', rooms);
   userUpdateNotification(room);
+  await setRedisData('roomData', rooms);
 };
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
@@ -441,15 +504,6 @@ const usePotion = async (
   room: Room,
   cardsType: number
 ) => {
-  // 스탯 데이터가 존재하는지 검사
-  const characterStats: { [roomId: number]: { [userId: number]: Character } } | undefined =
-    await getRedisData('userStatusData');
-  if (characterStats === undefined) {
-    console.error('userStatusData 정보가 존재하지 않습니다.');
-    return;
-  }
-  const characterStat = characterStats[room.id][user.id];
-
   // 해당 아이템 보유여부 검사
   let isOwned: boolean = false;
   for (let i = 0; i < user.character.handCards.length; i++) {
@@ -466,8 +520,100 @@ const usePotion = async (
   } else {
     user.character.hp = user.character.maxHp;
   }
-  characterStat.mp += restoreMp;
-  await setRedisData('userStatusData', characterStats);
+  user.character.mp += restoreMp;
+  await setRedisData('roomData', rooms);
+  userUpdateNotification(room);
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+// 방어구 장착 equipType (0: 투구, 1: 갑옷, 2: 망토, 3: 장갑)
+const equipItem = async (user: User, rooms: Room[], room: Room, equipIndex: number, cardsType: number) => {
+  // 해당 아이템 보유여부 검사
+  let isOwned: boolean = false;
+  for (let i = 0; i < user.character.handCards.length; i++) {
+    if (user.character.handCards[i].type === cardsType && user.character.handCards[i].count > 0) {
+      user.character.handCards[i].count--;
+      isOwned = true;
+    }
+  }
+  if (isOwned === false) return;
+
+  // equipIndex 검사
+  if (equipIndex !== 0 && equipIndex !== 1 && equipIndex !== 2 && equipIndex !== 3) {
+    console.log('올바르지 않은 equipIndex입니다.');
+  }
+
+  // 장착중이던 방어구 해제
+  if (![302, 303, 304, 305].includes(user.character.equips[equipIndex]) && user.character.equips[equipIndex]) {
+    // 장착칸 → handCards로 옮기기
+    let isInHandCards: boolean = false;
+    for (let i = 0; i < user.character.handCards.length; i++) {
+      if (user.character.handCards[i].type === user.character.equips[equipIndex]) {
+        user.character.handCards[i].count++;
+        isInHandCards = true;
+        break;
+      }
+    }
+    if (isInHandCards === false) user.character.handCards.push({ type: user.character.equips[equipIndex], count: 1 });
+
+    // 능력치 하락
+    user.character.attack -= DBEquip[user.character.equips[equipIndex]].attack;
+    user.character.armor -= DBEquip[user.character.equips[equipIndex]].armor;
+    user.character.maxHp -= DBEquip[user.character.equips[equipIndex]].hp;
+    user.character.hp -= DBEquip[user.character.equips[equipIndex]].hp;
+  }
+
+  // 새로운 방어구 장착 및 능력치 상승
+  user.character.equips[equipIndex] = cardsType;
+  user.character.attack += DBEquip[cardsType].attack;
+  user.character.armor += DBEquip[cardsType].armor;
+  user.character.maxHp += DBEquip[cardsType].hp;
+  user.character.hp += DBEquip[cardsType].hp;
+
+  await setRedisData('roomData', rooms);
+  userUpdateNotification(room);
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+// 무기 장착
+const equipWeapon = async (user: User, rooms: Room[], room: Room, cardsType: number) => {
+  // 해당 아이템 보유여부 검사
+  let isOwned: boolean = false;
+  for (let i = 0; i < user.character.handCards.length; i++) {
+    if (user.character.handCards[i].type === cardsType && user.character.handCards[i].count > 0) {
+      user.character.handCards[i].count--;
+      isOwned = true;
+    }
+  }
+  if (isOwned === false) return;
+
+  // // 장착중이던 무기 해제
+  if (user.character.weapon !== 301 && user.character.weapon) {
+    // 장착칸 → handCards로 옮기기
+    let isInHandCards: boolean = false;
+    for (let i = 0; i < user.character.handCards.length; i++) {
+      if (user.character.handCards[i].type === user.character.weapon) {
+        user.character.handCards[i].count++;
+        isInHandCards = true;
+        break;
+      }
+    }
+    if (isInHandCards === false) user.character.handCards.push({ type: user.character.weapon, count: 1 });
+
+    // 능력치 하락
+    user.character.attack -= DBEquip[user.character.weapon].attack;
+    user.character.armor -= DBEquip[user.character.weapon].armor;
+    user.character.maxHp -= DBEquip[user.character.weapon].hp;
+    user.character.hp -= DBEquip[user.character.weapon].hp;
+  }
+
+  // 새로운 무기 장착 및 능력치 상승
+  user.character.weapon = cardsType;
+  user.character.attack += DBEquip[cardsType].attack;
+  user.character.armor += DBEquip[cardsType].armor;
+  user.character.maxHp += DBEquip[cardsType].hp;
+  user.character.hp += DBEquip[cardsType].hp;
+
   await setRedisData('roomData', rooms);
   userUpdateNotification(room);
 };
