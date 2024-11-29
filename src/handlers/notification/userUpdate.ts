4d9d1d3af9d0b6d19faf0ef92e35fcf584f6adb3
applaud13 <applaud13@gmail.com>
2024-11-29 18:57:40 +0900

    refactor userUpdate
    merge전에 주석처리했던 코드 실수로 주석 뺀것 다시 넣기

--- src/handlers/notification/userUpdate.ts
+++ src/handlers/notification/userUpdate.ts
@@ -22,18 +22,18 @@ export const userUpdateNotification = async (room: Room | null) => {
   });
 
   // 쇼핑중인 유저들에게 쇼핑 화면 다시 보여주기
-  // let redisFleaMarketCards: { [roomId: number]: number[] } | undefined = await getRedisData('fleaMarketCards');
-  // if (!redisFleaMarketCards) {
-  //   console.error('fleaMarketItemSelect 레디스에 상점 카드가 없음');
-  //   return;
-  // }
-  // for (let i = 0; i < shoppingUserIdSessions[room.id].length; i++) {
-  //   const shoppingUserSocket = socketSessions[shoppingUserIdSessions[room.id][i]];
-  //   sendPacket(shoppingUserSocket, config.packetType.FLEA_MARKET_PICK_RESPONSE, {
-  //     fleaMarketCardTypes: redisFleaMarketCards[room.id]
-  //   });
-  //   console.log(`유저ID: ${shoppingUserIdSessions[room.id][i]}에게 플리마켓 보냄`);
-  // }
+  let redisFleaMarketCards: { [roomId: number]: number[] } | undefined = await getRedisData('fleaMarketCards');
+  if (!redisFleaMarketCards) {
+    console.error('fleaMarketItemSelect 레디스에 상점 카드가 없음');
+    return;
+  }
+  for (let i = 0; i < shoppingUserIdSessions[room.id].length; i++) {
+    const shoppingUserSocket = socketSessions[shoppingUserIdSessions[room.id][i]];
+    sendPacket(shoppingUserSocket, config.packetType.FLEA_MARKET_PICK_RESPONSE, {
+      fleaMarketCardTypes: redisFleaMarketCards[room.id]
+    });
+    console.log(`유저ID: ${shoppingUserIdSessions[room.id][i]}에게 플리마켓 보냄`);
+  }
 
   return;
 };
