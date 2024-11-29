9e0f18e195cc746b2e8fcf23e9a845ed98ac292a
성원 정 <tyhg56@naver.com>
2024-11-29 20:36:11 +0900

    fix : " ChattingRoom 수정 "
    ChattingRoom 수정
     - id ( 방을 구분짓기 위한 아이디 )
     - ownerEmail ( 채팅방 방장 )
     - users ( 채팅방에 참여중인 유저 목록 )
     - maxUser ( 채팅방 최대 참여 유저 크기 )
    
     - 생성자 수정
      - 아이디, 방장 이메일, 유저 목록 배열 초기화, 최대 참여 유저 크기 저장
    
     - getRoomId()
      - 방 아이디 반환
    
     - getOwnerEmail()
      - 방장 이메일 반환
    
     - roomUserAdd() 추가
      - 방에 유저 추가
    
     - roomUserDelete() 추가
      - 방에서 유저 삭제
    
     - update() 방 업데이트

--- src/chattingServer/class/chattingRoom.ts
+++ src/chattingServer/class/chattingRoom.ts
@@ -1,6 +1,46 @@
-class ChattingRoom {
-    constructor() {
+import { User } from "../../interface/interface.js";
 
+class ChattingRoom {
+    private id: number;
+    private ownerEmail: string;
+    private users: User[];
+    private maxUser: number;
+
+    constructor(id: number, ownerEmail: string, maxUser: number) {
+        this.id = id;
+        this.ownerEmail = ownerEmail;
+        this.users = [];
+        this.maxUser = maxUser;
+    }
+
+    getRoomId() {
+        return this.id;
+    }
+
+    getOwnerEmail() {
+        return this.ownerEmail;
+    }
+
+    roomUserAdd(user: User) {
+        if (this.users.length === this.maxUser) {
+            console.error("ChattingRoom roomUserAdd 방에 유저가 가득참");
+            return;
+        }
+
+        this.users.push(user);
+    }
+
+    roomUserDelete(id: number) {
+        if (this.users.length === 0) {
+            console.error("ChattingRoom roomUserDelete 방에 유저가 없는데 삭제 하려고 들어옴");
+            return;
+        }
+
+        this.users = this.users.filter((user) => user.id !== id);
+    }
+
+    update() {
+        //console.log(`${this.id} 방 업데이트`);
     }
 }
 
