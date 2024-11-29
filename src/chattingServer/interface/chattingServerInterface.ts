771aba768f000e4ce97e8f2ddebc34c2b9586c33
성원 정 <tyhg56@naver.com>
2024-11-29 20:40:18 +0900

    fix : " chattingServerInterface 수정 "
    chattingServerInterface
     - Job 추가
      - 채팅 서버에서 사용할 Job
    
     - SendPacketData 추가
      - 워커가 사용할 데이터

--- src/chattingServer/interface/chattingServerInterface.ts
+++ src/chattingServer/interface/chattingServerInterface.ts
@@ -1,8 +1,34 @@
-export interface ChattingLoginRequest {
+import { CustomSocket } from "../../interface/interface.js";
+
+export class Job {
+    public jobType: number;
+    public payload: (string | number | boolean | object)[];
+    constructor(jobType: number, ...payload: (string | number | boolean | object)[]) {
+        this.jobType = jobType;
+        this.payload = payload;
+    }
+}
+
+export class SendPacketData {
+    //public socket: CustomSocket;
+    public packetType: number;
+    public data: Object;
+    constructor(packetType: number, data: Object) {
+        //this.socket = socket;
+        this.packetType = packetType;
+        this.data = data;
+    }
+}
+
+export interface ChattingLoginRequestPayload {
     email: string;
 }
 
 export interface ChattingLoginResponse {
     success: boolean;
     message: string;
+}
+
+export interface ChattingCreateRoomRequestPayload {
+    email: string;
 }
