594efc7a08fb87844d793d7938f0c74bb32bf5c6
성원 정 <tyhg56@naver.com>
2024-11-30 17:16:10 +0900

    docs : " 채팅서버 프로토콜 수정 "
    chattingProtocol.proto
     - C2SChattingServerCreateRoomRequest 수정
      - email 삭제
    
     - C2SChattingServerJoinRoomRequest 추가 ( 방 참가 )
      - chattingRoomId ( 참가할 방 번호 )
      - email ( 참가하는 유저 이메일 )
    
     - S2CChattingServerCreateRoomResponse 추가 ( 방 생성 응답 )
      - chattingRoomId ( 생성한 방 번호 )

--- src/chattingProtobuf/chattingPacketNames.ts
+++ src/chattingProtobuf/chattingPacketNames.ts
@@ -4,9 +4,11 @@ export const chattingPacketNames = {
     },
     request: {
         C2SChattingServerLoginRequest: 'request.C2SChattingServerLoginRequest',
-        C2SChattingServerCreateRoomRequest: 'request.C2SChattingServerCreateRoomRequest'
+        C2SChattingServerCreateRoomRequest: 'request.C2SChattingServerCreateRoomRequest',
+        C2SChattingServerJoinRoomRequest: 'reqeust.C2SChattingServerJoinRoomRequest'
     },
     response: {
-        S2CChattingServerLoginResponse: 'response.S2CChattingServerLoginResponse'
+        S2CChattingServerLoginResponse: 'response.S2CChattingServerLoginResponse',
+        S2CChattingServerCreateRoomResponse: 'response.S2CChattingServerCreateRoomResponse'
     }
 }
