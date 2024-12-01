d425c6e891259983af412aaa0c13cb135022012a
성원 정 <tyhg56@naver.com>
2024-12-01 21:17:48 +0900

    add : " 채팅방 참여, 채팅방 떠나기 패킷 처리 추가 "
    chattingHandlerIndex
     - 채팅방 참가, 채팅방 떠나기 핸들러 할당
    
    chattingJoinRoomReqHandler
     - 채팅방 참여 요청 오면 JOIN_ROOM_JOB 생성 해서 채팅서버에 인큐
    
    chattingLeaveRoomReqHandler
     - 채팅방 떠나기 요청 오면 LEAVE_ROOM_JOB 생성 해서 채팅서버에 인큐

--- src/chattingServer/handlers/packetHandlers/room/chattingJoinRoomReqHandler.ts
+++ src/chattingServer/handlers/packetHandlers/room/chattingJoinRoomReqHandler.ts
@@ -1,5 +1,20 @@
+import { config } from "../../../../config/config.js";
 import { CustomSocket } from "../../../../interface/interface.js";
+import ChattingServer from "../../../class/chattingServer.js";
+import { ChattingJoinRoomRequestPayload, Job } from "../../../interface/chattingServerInterface.js";
 
 // 채팅 방 참여 요청
 export const chattingJoinRoomReqHandler = async (socket: CustomSocket, payload: Object): Promise<void> => {
+    console.log("채팅 방 참여 요청");
+
+    const chattingUser = ChattingServer.getInstance().getUserBySocket(socket);
+    if (chattingUser === undefined) {
+        return;
+    }
+
+    const chattingJoinRoomPayload = payload as ChattingJoinRoomRequestPayload;
+
+    const chattingJoinRoomJob = new Job(config.jobType.CHATTING_JOIN_ROOM_REQUEST_JOB, chattingUser, chattingJoinRoomPayload.ownerEmail);
+
+    ChattingServer.getInstance().chattingServerJobQue.push(chattingJoinRoomJob);
 }
