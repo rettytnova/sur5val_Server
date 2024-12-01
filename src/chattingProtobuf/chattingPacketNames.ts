a52a772651d0921e10e04232013127067a6926bc
성원 정 <tyhg56@naver.com>
2024-12-01 14:13:04 +0900

    docs : " chattingPacketNames.ts 수정 "
    chattingPacketNames
     - chattingPacket -> ChattingPacket 이름 변경
     - reqeust -> request 오타 수정

--- src/chattingProtobuf/chattingPacketNames.ts
+++ src/chattingProtobuf/chattingPacketNames.ts
@@ -1,11 +1,11 @@
 export const chattingPacketNames = {
     packet: {
-        chattingPacket: 'packet.ChattingPacket'
+        ChattingPacket: 'packet.ChattingPacket'
     },
     request: {
         C2SChattingServerLoginRequest: 'request.C2SChattingServerLoginRequest',
         C2SChattingServerCreateRoomRequest: 'request.C2SChattingServerCreateRoomRequest',
-        C2SChattingServerJoinRoomRequest: 'reqeust.C2SChattingServerJoinRoomRequest'
+        C2SChattingServerJoinRoomRequest: 'request.C2SChattingServerJoinRoomRequest'
     },
     response: {
         S2CChattingServerLoginResponse: 'response.S2CChattingServerLoginResponse',
