2c15315c39f6ce60f6eadcdf30a7d18b5d84dbcc
성원 정 <tyhg56@naver.com>
2024-12-01 16:58:35 +0900

    add : " ChattingJoinRoomHandler, ChattingJoinRoomJobHandler 추가 "
    ChattingJoinRoomHandler ( 채팅 방 참가 요청 ) 추가
    
    ChattingJoinRoomJobHandler ( 채팅 방 참가 요청 잡 처리 ) 추가

---
+++ src/chattingServer/handlers/jobHandlers/room/chattingJoinRoomJobHandler.ts
@@ -0,0 +1,5 @@
+import { Job } from "../../../interface/chattingServerInterface.js";
+
+// 채팅 방 참여 
+export const chattingJoinRoomJobHandler = (job: Job): void => {
+}
---
+++ src/chattingServer/handlers/packetHandlers/room/chattingJoinRoomReqHandler.ts
@@ -0,0 +1,5 @@
+import { CustomSocket } from "../../../../interface/interface.js";
+
+// 채팅 방 참여 요청
+export const chattingJoinRoomReqHandler = async (socket: CustomSocket, payload: Object): Promise<void> => {
+}
