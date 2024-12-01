858a761f3bc1007d2242fe1ee89a0aaf40f1ca16
성원 정 <tyhg56@naver.com>
2024-12-01 21:22:03 +0900

    add : " 채팅방 참여, 떠나기 job 처리 추가 "
    jobHandlerIndex 수정
     - 채팅방 떠나기 job 처리 핸들러 할당
    
    chattingJoinRoomJobHandler 수정
     - 참여할 채팅방 찾아서 요청한 유저 추가
    
    chattingLeaveRoomJobHandler 추가
      - 떠날 채팅방 찾아서 요청한 유저 삭제
    
    chattingLeaveRoomJobHandler 추가

--- src/chattingServer/handlers/jobHandlers/room/chattingJoinRoomJobHandler.ts
+++ src/chattingServer/handlers/jobHandlers/room/chattingJoinRoomJobHandler.ts
@@ -1,5 +1,19 @@
+import ChattingServer from "../../../class/chattingServer.js";
+import ChattingUser from "../../../class/chattingUser.js";
 import { Job } from "../../../interface/chattingServerInterface.js";
 
-// 채팅 방 참여 
+// 채팅 방 참여
 export const chattingJoinRoomJobHandler = (job: Job): void => {
+    const chattingJoinRoomUser = job.payload[0] as ChattingUser;
+    const chattingRoomOwnerEmail = job.payload[1] as string;
+
+    const chattingRoom = ChattingServer.getInstance().getRoomByOwnerEmail(chattingRoomOwnerEmail);
+    if (chattingRoom === undefined) {
+        console.log("참여할 채팅 방이 없음");
+        return;
+    }
+
+    chattingJoinRoomUser.setJoinRoomId(chattingRoom.getRoomId());
+
+    chattingRoom.roomUserAdd(chattingJoinRoomUser);
 }
