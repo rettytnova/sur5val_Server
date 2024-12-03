0a8dc4907d5b8173bb0258ff81ccd1a22e80d395
성원 정 <tyhg56@naver.com>
2024-12-03 09:35:03 +0900

    fix : " ChattingProtocol 수정 "
    ChattingProtocol
     - C2SChattingServerChatSendRequest 추가 ( 채팅 전송 요청 )
     - S2CChattingServerChatSendResponse 추가 ( 채팅 전송 요청 응답 )

--- src/chattingProtobuf/chattingPacketNames.ts
+++ src/chattingProtobuf/chattingPacketNames.ts
@@ -6,10 +6,12 @@ export const chattingPacketNames = {
         C2SChattingServerLoginRequest: 'request.C2SChattingServerLoginRequest',
         C2SChattingServerCreateRoomRequest: 'request.C2SChattingServerCreateRoomRequest',
         C2SChattingServerJoinRoomRequest: 'request.C2SChattingServerJoinRoomRequest',
-        C2SChattingServerLeaveRoomRequest: 'request.C2SChattingServerLeaveRoomRequest'
+        C2SChattingServerLeaveRoomRequest: 'request.C2SChattingServerLeaveRoomRequest',
+        C2SChattingServerChatSendRequest: 'request.C2SChattingServerChatSendRequest'
     },
     response: {
         S2CChattingServerLoginResponse: 'response.S2CChattingServerLoginResponse',
-        S2CChattingServerCreateRoomResponse: 'response.S2CChattingServerCreateRoomResponse'
+        S2CChattingServerCreateRoomResponse: 'response.S2CChattingServerCreateRoomResponse',
+        S2CChattingServerChatSendResponse: 'response.S2CChattingServerChatSendResponse'
     }
 }
