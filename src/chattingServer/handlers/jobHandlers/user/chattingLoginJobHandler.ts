dcdfd4a6545c3a136fcb286197a75a0b69711f74
성원 정 <tyhg56@naver.com>
2024-12-03 09:45:55 +0900

    fix : " chattingLoginJobHandler 수정 "
    chattingLoginJobHandler
     - DB에서 유저를 가져와서 유저 닉네임을 저장

--- src/chattingServer/handlers/jobHandlers/user/chattingLoginJobHandler.ts
+++ src/chattingServer/handlers/jobHandlers/user/chattingLoginJobHandler.ts
@@ -1,13 +1,21 @@
+import DatabaseManager from "../../../../database/databaseManager.js";
 import { CustomSocket } from "../../../../interface/interface.js";
 import ChattingServer from "../../../class/chattingServer.js";
 import ChattingUser from "../../../class/chattingUser.js";
 import { Job } from "../../../interface/chattingServerInterface.js";
 
 // 채팅 서버 로그인
-export const chattingLoginJobHandler = (job: Job): void => {
+export const chattingLoginJobHandler = async (job: Job): Promise<void> => {
     const userEmail = job.payload[0] as string;
     const userSocket = job.payload[1] as CustomSocket;
 
-    const newChattingUser = new ChattingUser(userSocket, userEmail);
+    const user: any = await DatabaseManager.getInstance().findUserByEmail(userEmail);
+    if (!user) {
+        return;
+    }
+
+    const loginUserNickName = user.nickname as string;
+
+    const newChattingUser = new ChattingUser(userSocket, userEmail, loginUserNickName);
     ChattingServer.getInstance().getUsers().push(newChattingUser);
 }
