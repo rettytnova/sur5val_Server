1605c2465e37bdbbe4de520738601bff146ed93e
성원 정 <tyhg56@naver.com>
2024-12-04 12:43:15 +0900

    fix : " chattingOnEnd 수정 "
    chattingOnEnd
     - 유저가 나가면 채팅방에서 나감

--- src/chattingServer/events/chattingOnEnd.ts
+++ src/chattingServer/events/chattingOnEnd.ts
@@ -1,5 +1,13 @@
 import { CustomSocket } from "../../interface/interface.js";
+import ChattingServer from "../class/chattingServer.js";
+import { chattingLeaveRoomReqHandler } from "../handlers/packetHandlers/room/chattingLeaveRoomReqHandler.js";
 
 export const chattingOnEnd = (socket: CustomSocket) => async () => {
+    const chattingServerLeaveUser = ChattingServer.getInstance().getUserBySocket(socket);
+    if (!chattingServerLeaveUser) {
+        console.log("chattingOnEnd User 없음");
+        return;
+    }
 
+    chattingLeaveRoomReqHandler(socket, {});
 };
