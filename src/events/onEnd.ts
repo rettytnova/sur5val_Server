d65dc211f59d45ce0f633f027f9735ae90047f4e
applaud13 <applaud13@gmail.com>
2024-12-08 17:59:47 +0900

    fix project
    불 필요한 console.log 삭제

--- src/events/onEnd.ts
+++ src/events/onEnd.ts
@@ -1,5 +1,3 @@
 import { CustomSocket } from '../interface/interface.js';
 
-export const onEnd = (socket: CustomSocket) => async () => {
-  console.log('onEnd 함수에 누구 들어옴');
-};
+export const onEnd = (socket: CustomSocket) => async () => {};
