8d557e5a5f749896f42654c42659f88228914612
성원 정 <tyhg56@naver.com>
2024-12-22 18:09:23 +0900

    fix : " dummyClient 수정 "
    dummyClient
     - 로그인을 해야 게임을 시작할 수 있도록함
     - 게임 시작 하면 방장이 방을 생성하고, 나머지 더미들은 랜덤하게 방에 입장

--- src/dummyClient/responseHandlers/game/gamePrepareNotificationHandler.ts
+++ src/dummyClient/responseHandlers/game/gamePrepareNotificationHandler.ts
@@ -1,5 +1,29 @@
-import { UserClient } from "../../dummyClient.js"
+import Long from "long";
+import { Room } from "../../../gameServer/interface/interface.js"
+import { gDummyClients, UserClient } from "../../dummyClient.js"
 
-export const gamePrepareNotificationHandler = (userClient: UserClient, payload: Object): void => {
+export const gamePrepareNotificationHandler = (userClient: UserClient, payload: any): void => {
+    const room: Room = payload.room as Room;
+    //console.log(`게임 준비 완료 방 id ${room.id} ownerId ${room.ownerId}`);
 
+    const leaderDummy = gDummyClients.find((dummy: UserClient) => {
+        const dummyId: Long = Long.fromNumber(dummy.getId());
+        const ownerId: Long = Long.fromNumber(room.ownerId);
+
+        return dummyId.equals(ownerId);
+    });
+
+    if (leaderDummy) {
+        const leaderDummyId: Long = Long.fromNumber(leaderDummy.getId());
+        const userId: Long = Long.fromNumber(userClient.getId());
+
+        if (leaderDummyId.equals(userId)) {
+            leaderDummy.GameServerGameStart();
+        }
+
+        userClient.DummyRandomMoveStart();
+    }
+    else {
+        console.log(`gamePrepareNoti 방장 더미가 없음`);
+    }
 }
